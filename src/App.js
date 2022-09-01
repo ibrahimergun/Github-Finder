d9f9641b863ade66197a521c0fc7c4e712f2c7b7
İbrahim Ergün <ibrahimergun19@gmail.com>
2022-09-01 23:11:17 +0200

    Github finder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div class='min-h-screen w-full bg-white'>
+      <div className='bg-purple-500'>
+        <h1 className='text-xl'>Hello World</h1>
+        <button className='btn'>Click</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
